77372fd9d26b1c18e763a4e30c734c1481d6e4ce
Nikolai <n.dunkel@gmail.com>
2019-12-01 01:01:28 -0800

    labels integrated

--- src/ComparerWidget/index.js
+++ src/ComparerWidget/index.js
@@ -16,23 +16,23 @@ export default function ComparerWidget(props) {
                 {/* <p style={{color:'grey'}}>{ props.item.price }</p> */}
                 {/* <p style="color:grey">{props.item.sust1}</p> */}
                 {props.item.greene ? <p style={{display:'inline'}}><a target="_blank" href="www.green-e.org"><img src="green-e.gif" height="30px"/></a></p> : null } 
-                {/* <p *ngIf="new.value.cert == 'USDA Certified Organic'" style="display:inline"><a target="_blank" href="https://www.ams.usda.gov/rules-regulations/organic"><img src="assets/organic.gif" height="30px"></a></p>
-                <p *ngIf="new.value.cert == 'Fair Trade Certified'" style="display:inline"><a target="_blank" href="http://fairtradeusa.org/cooplink"><img src="assets/fairtrade.gif" height="30px"></a></p>
-                <p *ngIf="new.value.cert == 'B-Corp'" style="display:inline"><a target="_blank" href="https://www.bcorporation.net"><img src="assets/bcorp.gif" height="30px"></a></p>
-                <p *ngIf="new.value.cert == 'Carbon-Free'" style="display:inline"><a target="_blank" href="https://carbonfund.org/product-certification"><img src="assets/carbonfree.gif" height="30px"></a></p> */}
+                {props.item.organic ? <p style={{display:'inline'}}><a target="_blank" href="https://www.ams.usda.gov/rules-regulations/organic"><img src="organic.gif" height="30px"/></a></p> : null } 
+                {props.item.fairtrade ? <p style={{display:'inline'}}><a target="_blank" href="http://fairtradeusa.org/cooplink"><img src="fairtrade.gif" height="30px"/></a></p> : null } 
+                {props.item.bcorp ? <p style={{display:'inline'}}><a target="_blank" href="https://www.bcorporation.net"><img src="bcorp.gif" height="30px"/></a></p> : null } 
+                {props.item.carbonfree ? <p style={{display:'inline'}}><a target="_blank" href="https://carbonfund.org/product-certification"><img src="carbonfree.gif" height="30px"/></a></p> : null } 
                 {props.item.closedloop ? <p style={{display:'inline'}}><img src="closedloop.gif" height="30px"/></p> : null }
-            {/* <p *ngIf="new.value.cert == 'Fair Labor Certified'" style="display:inline"><a target="_blank" href="http://www.fairlabor.org"><img src="assets/fairlabor1.gif" height="30px"></a></p> */}
-                {props.item.fairtrade ? <p style={{display:'inline'}}><a target="_blank" href="http://fairtradeusa.org/certification"><img src="fairlabor2.gif" height="30px" /></a></p> : null }
-                 {/* <p *ngIf="new.value.cert == 'Local Production'" style="display:inline"><img src="assets/local.gif" height="30px"></p>
-            <!-- 	<p *ngIf="new.value.noncarbon" style="display:inline"><img src="assets/noncarbon.gif" height="30px"></p> -->
-                <p *ngIf="new.value.cert == 'PETA Cruelty Free'" style="display:inline"><a target="_blank" href="http://www.peta.org/about-peta/how-is-a-company-certified-as-cruelty-free/"><img src="assets/noncruelty.gif" height="30px"></a></p>
-            <!-- 	<p *ngIf="new.value.nonfur" style="display:inline"><a target="_blank" href="http://www.peta.org/about-peta/how-is-a-company-certified-as-cruelty-free/"><img src="assets/nonfur.gif" height="30px"></a></p> -->
-                <p *ngIf="new.value.cert == 'GMO-Free'" style="display:inline"><img src="assets/nongmo.gif" height="30px"></p>
-                <p *ngIf="new.value.cert == 'EWG A'" style="display:inline"><img src="assets/nontoxic.gif" height="30px"></p>
-                <!-- <p *ngIf="new.value.nonwaste" style="display:inline"><img src="assets/nonwaste.gif" height="30px"></p> -->
-                <p *ngIf="new.value.cert == 'Vegan'" style="display:inline"><img src="assets/vegan.gif" height="30px"></p>
-                <p *ngIf="new.value.cert == 'None'" style="display:inline; color:grey; height:30px">No Certifications</p> */}
-            <br />
+                {props.item.fairlabor1 ? <p style={{display:'inline'}}><a target="_blank" href="http://www.fairlabor.org"><img src="fairlabor1.gif" height="30px" /></a></p> : null }
+                {props.item.fairlabor2 ? <p style={{display:'inline'}}><a target="_blank" href="http://fairtradeusa.org/certification"><img src="fairlabor2.gif" height="30px" /></a></p> : null }
+                {props.item.local ? <p style={{display:'inline'}}><img src="local.gif" height="30px"/></p> : null }
+                {props.item.noncarbon ? <p style={{display:'inline'}}><img src="noncarbon.gif" height="30px"/></p> : null }
+                {props.item.noncruelty ? <p style={{display:'inline'}}><a target="_blank" href="http://www.peta.org/about-peta/how-is-a-company-certified-as-cruelty-free/"><img src="noncruelty.gif" height="30px"/></a></p> : null }
+                {props.item.nonfur ? <p style={{display:'inline'}}><a target="_blank" href="http://www.peta.org/about-peta/how-is-a-company-certified-as-cruelty-free/"><img src="nonfur.gif" height="30px"/></a></p> : null }
+                {props.item.nongmo ? <p style={{display:'inline'}}><img src="nongmo.gif" height="30px"/></p> : null }
+                {props.item.nontoxic ? <p style={{display:'inline'}}><img src="nontoxic.gif" height="30px"/></p> : null }
+                {props.item.nonwaste ? <p style={{display:'inline'}}><img src="nonwaste.gif" height="30px"/></p> : null }
+                {props.item.vegan ? <p style={{display:'inline'}}><img src="vegan.gif" height="30px"/></p> : null }
+                {props.item.none ? <p style={{display:'inline'}}>No Certification</p> : null }
+                <br />
                 <button className="btn btn-outline-dark btn-sm pull-right">
                     { props.item.upvote } Purchased Through Superjuice
                 </button>
