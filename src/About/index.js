548fe21194d634c2809f9a3aae0c98ead1bc41ff
Nikolai <n.dunkel@gmail.com>
2019-12-01 00:00:27 -0800

    working with previews

--- src/About/index.js
+++ src/About/index.js
@@ -7,11 +7,11 @@ export default function About() {
             <div style={{paddingTop:100, paddingBottom:100}}>
                 <img src="superjuicelogo.png" height="80px" style={{margin: 20}} />
                 <h3>
-                    Hi! We think buying sustainably is too expensive and time-consuming.
+                    Hi!<br /> We think sustainable buying is too expensive and time-consuming.
                 </h3>
                 <br />
                 <hr />
-                    <h3>Superjuice tries to solve this problem.</h3>
+                    <h5>Superjuice tries to solve this problem.</h5>
                     <p>Inspired by Stewart Brand's <a href="https://en.wikipedia.org/wiki/Whole_Earth_Catalog">Whole Earth Catalog</a>, Superjuice is a price and certification comparison engine.</p>
                     <p><b>Disclaimer:</b> When readers choose to buy from some (but not all) of the listed companies in the catalog, we earn affiliate commissions that support our work.</p>
                     <p>For any questions or comments, email Niko at n.dunkel (at) gmail.com </p> 
--- src/ComparerList/index.js
+++ src/ComparerList/index.js
@@ -39,7 +39,7 @@ export default function ComparerList() {
         return (
                 <div>
                         <div style={{ textAlign: 'center', paddingTop: 20, paddingBottom: 20 }}>
-                                <h3 style={{ textAlign: 'center' }}>Where To Get Affordable, Sustainable {category}</h3>
+                                <h3 style={{ textAlign: 'center' }}>Where To Get Affordable, Sustainable <u>{category}</u></h3>
                                 <br />
                                 <div className="myPanel">
                                         <div className="btn-group btn-group-justified " style={{ display: 'flex', flexWrap: 'wrap' }}>
